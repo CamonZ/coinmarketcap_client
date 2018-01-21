da13a1d6420601041f40cb9b6c90a82dce93c115
Simon Garcia <simon.garciar@gmail.com>
2018-01-21 06:46:10 +0100

    Add function to retrieve a single coin info

--- lib/coinmarketcap_client.ex
+++ lib/coinmarketcap_client.ex
@@ -15,5 +15,9 @@ defmodule CoinmarketcapClient do
     get("/v1/ticker", query: [start: start, limit: limit]).body
   end
 
+  def coin_summary(cmc_name) do
+    get("/v1/ticker/#{cmc_name}").body
+  end
+
   def all_coins(), do: coins_summary(0, 0)
 end
